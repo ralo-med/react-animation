9bae734500d9d400973bdde1841c27fc46c22be8
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-17 07:56:10 +0900

    #Var1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import type { Variants } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -17,19 +18,21 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVariants: Variants = {
+  start: { scale: 0 },
+  end: {
+    scale: 1,
+    rotateZ: 360,
+    transition: {
+      type: "spring",
+    },
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{
-          scale: 0,
-        }}
-        animate={{
-          scale: 1,
-          rotateZ: 360,
-        }}
-        transition={{ type: "spring", delay: 1 }}
-      />
+      <Box variants={myVariants} initial="start" animate="end" />
     </Wrapper>
   );
 }
